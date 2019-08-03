09ad3a41aa066d44507377a2aa6c0ecaa494932c
Maks <petrenkomaks405@gmail.com>
2019-08-03 19:38:41 +0300

    AddCross

---
+++ src/Cross.java
@@ -0,0 +1,13 @@
+public class Cross {
+    public static void main(String []args){
+        String A = "0010100";
+        String B = "0001000";
+        String C = "1000001";
+
+        System.out.println(C + " " + C);
+        System.out.println(A + " " + A);
+        System.out.println(B + " " + B);
+        System.out.println(A + " " + A);
+        System.out.println(C + " " + C);
+    }
+}

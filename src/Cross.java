875cb5501ae9c343ba47ee4cfe5839f550528e55
Maks <petrenkomaks405@gmail.com>
2019-08-07 22:14:23 +0300

    replace char

--- src/Cross.java
+++ src/Cross.java
@@ -21,5 +21,17 @@ public class Cross {
         System.out.print("Введіть символ на що замінити 2 = ");
         char new2 = s.next().charAt(0);
 
+        A = A.replace(old1, new1);
+        A = A.replace(old2, new2);
+        B = B.replace(old1, new1);
+        B = B.replace(old2, new2);
+        C = C.replace(old1, new1);
+        C = C.replace(old2, new2);
+
+        System.out.println(C + " " + C + "\n"
+                + A + " " + A + "\n"
+                + B + " " + B + "\n"
+                + A + " " + A + "\n"
+                + C + " " + C );
     }
 }

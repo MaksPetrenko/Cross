9839b6a4f3dffe0f1f5a2122e923ea6f5e638597
Maks <petrenkomaks405@gmail.com>
2019-08-09 22:50:57 +0300

    Replaced output to console for output to file (output)

--- src/Cross.java
+++ src/Cross.java
@@ -1,6 +1,10 @@
+import java.io.PrintWriter;
 import java.util.Scanner;
+
 public class Cross {
-    public static void main(String []args){
+    public static void main(String []args) throws Exception {
+        PrintWriter fw = new PrintWriter("output.txt");
+
         String A = "0010100";
         String B = "0001000";
         String C = "1000001";
@@ -9,7 +13,13 @@ public class Cross {
                 + A + " " + A + "\n"
                 + B + " " + B + "\n"
                 + A + " " + A + "\n"
-                + C + " " + C );
+                + C + " " + C);
+
+        fw.println(C + " " + C);
+        fw.println(A + " " + A);
+        fw.println(B + " " + B);
+        fw.println(A + " " + A);
+        fw.println(C + " " + C);
 
         Scanner s = new Scanner(System.in);
         System.out.print("Введіть символ що замінити 1 = ");
@@ -20,6 +30,11 @@ public class Cross {
         char new1 = s.next().charAt(0);
         System.out.print("Введіть символ на що замінити 2 = ");
         char new2 = s.next().charAt(0);
+
+        fw.println ("Символ що замінити 1 = " + old1);
+        fw.println ("Символ що замінити 2 = " + old2);
+        fw.println ("Символ на що замінити 1 = " + new1);
+        fw.println ("Символ на що замінити 2 = " + new2);
 
         A = A.replace(old1, new1);
         A = A.replace(old2, new2);
@@ -32,6 +47,19 @@ public class Cross {
                 + A + " " + A + "\n"
                 + B + " " + B + "\n"
                 + A + " " + A + "\n"
-                + C + " " + C );
+                + C + " " + C);
+
+        fw.println(C + " " + C);
+        fw.println(A + " " + A);
+        fw.println(B + " " + B);
+        fw.println(A + " " + A);
+        fw.println(C + " " + C);
+
+        fw.close();
     }
 }
+
+
+
+
+

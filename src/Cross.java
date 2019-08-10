cc33ff82c480df533c9fd5a0cd56d2606d45c42c
Maks <petrenkomaks405@gmail.com>
2019-08-10 14:02:20 +0300

    replaced the reading of 4 variables from the keyboard with the reading of 2 variables from the file

--- src/Cross.java
+++ src/Cross.java
@@ -1,9 +1,11 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.io.FileReader;
 
 public class Cross {
     public static void main(String []args) throws Exception {
         PrintWriter fw = new PrintWriter("output.txt");
+        FileReader fileReader = new FileReader("input.txt");
 
         String A = "0010100";
         String B = "0001000";
@@ -21,27 +23,20 @@ public class Cross {
         fw.println(A + " " + A);
         fw.println(C + " " + C);
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Введіть символ що замінити 1 = ");
-        char old1 = s.next().charAt(0);
-        System.out.print("Введіть символ що замінити 2 = ");
-        char old2 = s.next().charAt(0);
-        System.out.print("Введіть символ на що замінити 1 = ");
-        char new1 = s.next().charAt(0);
-        System.out.print("Введіть символ на що замінити 2 = ");
-        char new2 = s.next().charAt(0);
+        Scanner scan = new Scanner(fileReader);
 
-        fw.println ("Символ що замінити 1 = " + old1);
-        fw.println ("Символ що замінити 2 = " + old2);
+        char new1 = scan.nextLine().charAt(0);
+        char new2 = scan.nextLine().charAt(0);
+
         fw.println ("Символ на що замінити 1 = " + new1);
         fw.println ("Символ на що замінити 2 = " + new2);
 
-        A = A.replace(old1, new1);
-        A = A.replace(old2, new2);
-        B = B.replace(old1, new1);
-        B = B.replace(old2, new2);
-        C = C.replace(old1, new1);
-        C = C.replace(old2, new2);
+        A = A.replace('1', new1);
+        A = A.replace('0', new2);
+        B = B.replace('1', new1);
+        B = B.replace('0', new2);
+        C = C.replace('1', new1);
+        C = C.replace('0', new2);
 
         System.out.println(C + " " + C + "\n"
                 + A + " " + A + "\n"
@@ -56,6 +51,7 @@ public class Cross {
         fw.println(C + " " + C);
 
         fw.close();
+        fileReader.close();
     }
 }
 
